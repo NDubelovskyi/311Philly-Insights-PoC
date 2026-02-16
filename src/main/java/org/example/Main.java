2f2bb43a7b52c7c4168b58eb4a70e2fb60f2f8e2
Nazarii Dubelovskyi <tur78121@temple.edu>
2026-02-15 22:03:11 -0500

    Testing Google AI Studio API

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,19 @@
 package org.example;
+import com.google.genai.Client;
+import com.google.genai.types.GenerateContentResponse;
 
 public class Main {
     public static void main(String[] args) {
+        // Set up Google AI Studio API Key before using this program
+        // Replace Keys.GOOGLE_AI_STUDIO_API below with "YOUR_API" String
+        Client client = Client.builder().apiKey(Keys.GOOGLE_AI_STUDIO_API).build();
 
+        GenerateContentResponse response =
+                client.models.generateContent(
+                        "gemini-3-flash-preview",
+                        "In a 2 sentences, why there was a surge under 'Snow Removal' category in Philadelphia's 311 data report in January 2016",
+                        null);
+
+        System.out.println(response.text());
     }
-
 }
